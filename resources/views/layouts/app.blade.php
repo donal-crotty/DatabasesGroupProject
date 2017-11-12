60f6b80bf05e3f7ce591a32c8d841cd469aa1f27
DonalCrotty <donalcrotty15@gmail.com>
2017-11-12 15:17:34 +0000

    Auth added to PagesController

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'The Coopers Stash') }}</title>
+    <title>The Cooper's Stash</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
