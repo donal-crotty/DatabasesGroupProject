ee042d8b26a061bf8ee404d0a24f8c9f14e1bbf3
unknown <a00219437@Z205-PC14.studentnet.ait.ie>
2017-12-12 13:55:43 +0000

    document stuff

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -5,11 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Post;
+use DB;
 
 class ChartsController extends Controller
 {
 	public function showChartData(){
 		$posts = Post::all();
+		// $posts = DB::select("Select brand, product from `posts` GROUP BY `brand`,product");
+		// $posts = $posts->toArray();
+
+
+		// $brands = array();
+		// foreach($posts as $i => $v){
+		// 	var_dump($v);die();
+		// 	// if(!in_array($v['brand'], $brands)){
+		// 	// 	$brands.push($v['brand']);
+
+		// 	// }
+		// }
+		// var_dump($brands);die();
+
 		$posts = json_encode($posts);
 		return $posts;
 	}
